6c3354be7d0b4a3567346c7ebc9c34dee04d6d73
ridwanhasrisetiawan <ridwanhasrisetiawan@gmail.com>
2025-03-13 11:49:33 +0700

    Prosedur untk input

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -4,3 +4,25 @@ using namespace std;
 
 int arr[20];
 int n;
+
+void input(){
+    while(true){
+        cout << "Masukkan banyaknya elemen pada array :";
+        cin >> n;
+        if (n<=20)
+           break;
+        else{
+            cout << "\nArray dapat mempunyai maksimal 20 element\n" << endl;    
+        }   
+    }
+
+    cout << endl;
+    cout << "======================" << endl;
+    cout << "Masukkan element array" << endl;
+    cout << "======================" << endl;
+
+    for (int i - 0; i <n; i++){
+        cout << "Data ke-" << (i+1) << ";";
+        }
+
+}
